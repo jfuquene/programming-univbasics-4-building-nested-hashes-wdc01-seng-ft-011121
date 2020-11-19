2c561f21d37775f2f86449edf3b4131045357636
John Fuquene <jfuquene@email.wm.edu>
2020-11-19 21:47:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,12 @@ def monopoly_with_third_tier
     :three_piece_owned => 100,
     :four_piece_owned => 200
   }, 
-  :names => {}
+  :names => {
+    :reading_railroads => {},
+    :pennsylvania_railroads => {},
+    :b_and_o_railroad => {},
+    :shortline_railroad => {}
+  }
   }
     
   }
